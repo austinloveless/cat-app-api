e3aa5c0feababae81bcd1384db839468160652dd
austinloveless <austinloveless5171@gmail.com>
2018-07-13 09:20:18 -0600

    cat api

---
+++ api/cat-api.js
@@ -0,0 +1,47 @@
+module.exports = [
+  {
+    id: 1,
+    catImg: "https://googleimages.com/happy-emoticon.jpg",
+    subreddit: "startledCats"
+  },
+  {
+    id: 2,
+    catImg: "https://googleimages.com/sad-emoticon.jpg",
+    subreddit: "tuxedoCats"
+  },
+  {
+    id: 3,
+    catImg: "https://googleimages.com/happy-emoticon.jpg",
+    subreddit: "catsInSinks"
+  },
+  {
+    id: 4,
+    catImg: "https://googleimages.com/sad-emoticon.jpg",
+    subreddit: "catsInPants"
+  },
+  {
+    id: 5,
+    catImg: "https://googleimages.com/happy-emoticon.jpg",
+    subreddit: "catsWithJobs"
+  },
+  {
+    id: 6,
+    catImg: "https://googleimages.com/sad-emoticon.jpg",
+    subreddit: "catBellies"
+  },
+  {
+    id: 7,
+    catImg: "https://googleimages.com/sad-emoticon.jpg",
+    subreddit: "catPranks"
+  },
+  {
+    id: 8,
+    catImg: "https://googleimages.com/happy-emoticon.jpg",
+    subreddit: "catsAreAssholes"
+  },
+  {
+    id: 9,
+    catImg: "https://googleimages.com/happy-emoticon.jpg",
+    subreddit: "theCatDimension"
+  }
+];
---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const data = require("./api/cat-api");
+const port = parseInt(process.env.PORT || 8080);
+console.log(data);
+const app = express();
+app.use(
+  cors({
+    origin: true,
+    credentials: true
+  })
+);
+
+function getById(data, id) {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id == id) {
+      return data[i];
+    }
+  }
+  return null;
+}
+
+app.get("/", function(request, response) {
+  response.json({
+    data
+  });
+});
+
+app.get("/:id", function(request, response) {
+  var nerd = getById(data, request.params.id);
+  if (!nerd) {
+    response.status(404).json({
+      error: {
+        message: "No nerd found!"
+      }
+    });
+  } else {
+    response.json({
+      data: nerd
+    });
+  }
+});
+
+app.listen(port, () => console.log("Listening on http://localhost:8080/"));
