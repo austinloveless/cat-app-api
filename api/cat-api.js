449edd5a9230634d81496d4cf4435985b697631c
austinloveless <austinloveless5171@gmail.com>
2018-07-13 12:48:58 -0600

    added cat images

--- api/cat-api.js
+++ api/cat-api.js
@@ -1,47 +1,54 @@
 module.exports = [
   {
     id: 1,
-    catImg: "https://googleimages.com/happy-emoticon.jpg",
+    catImg:
+      "http://hellinahandbasket.net/wp-content/uploads/2013/10/startled-cat.jpg",
     subreddit: "startledCats"
   },
   {
     id: 2,
-    catImg: "https://googleimages.com/sad-emoticon.jpg",
+    catImg:
+      "https://image.shutterstock.com/image-photo/cat-tuxedo-260nw-272266700.jpg",
     subreddit: "tuxedoCats"
   },
   {
     id: 3,
-    catImg: "https://googleimages.com/happy-emoticon.jpg",
+    catImg:
+      "https://d2kwjcq8j5htsz.cloudfront.net/2015/04/no-manners-sink-cat.jpg",
     subreddit: "catsInSinks"
   },
   {
     id: 4,
-    catImg: "https://googleimages.com/sad-emoticon.jpg",
+    catImg:
+      "http://catnipsum.com/wp-content/uploads/2013/02/fat-cat-in-pants-small.jpg",
     subreddit: "catsInPants"
   },
   {
     id: 5,
-    catImg: "https://googleimages.com/happy-emoticon.jpg",
+    catImg:
+      "https://cdn.shopify.com/s/files/1/0344/6469/files/57435389c4618860478b4586_large.jpg?v=1485198841",
     subreddit: "catsWithJobs"
   },
   {
     id: 6,
-    catImg: "https://googleimages.com/sad-emoticon.jpg",
+    catImg:
+      "https://i.pinimg.com/736x/da/5f/99/da5f997d883a2e06967350427c3082d4--advanced-yoga-crazy-cats.jpg",
     subreddit: "catBellies"
   },
   {
     id: 7,
-    catImg: "https://googleimages.com/sad-emoticon.jpg",
+    catImg:
+      "http://youcef.esy.es/wp-content/uploads/zvfthumbs/img_45_funny-catsfunny-pranks-ultimate-funny-animals-compilation-march-2014-ep-15.jpg",
     subreddit: "catPranks"
   },
   {
     id: 8,
-    catImg: "https://googleimages.com/happy-emoticon.jpg",
+    catImg: "https://i.ytimg.com/vi/O75Gkz10W70/maxresdefault.jpg",
     subreddit: "catsAreAssholes"
   },
   {
     id: 9,
-    catImg: "https://googleimages.com/happy-emoticon.jpg",
+    catImg: "https://a.wattpad.com/cover/59793620-352-k594088.jpg",
     subreddit: "theCatDimension"
   }
 ];
